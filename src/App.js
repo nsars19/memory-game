33e65ef1a8674af93f3ab21c24c7545dc40d3929
Nick Sardonini <nsardonini@gmail.com>
2021-01-28 17:20:01 -0500

    add basic layout structure & state mgmt for score vars

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Card from "./components/Card";
+import cheddar from "./assets/cheddar.jpeg";
+import swiss from "./assets/swiss.jpeg";
+import american from "./assets/american.jpeg";
+import colby from "./assets/colby.jpeg";
+import mozz from "./assets/mozz.jpeg";
+import parm from "./assets/parmesan.jpeg";
+import bleu from "./assets/bleu.jpeg";
+import brie from "./assets/brie.jpeg";
+import ricotta from "./assets/ricotta.jpeg";
 
 function App() {
+  const [highScore, setHighScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+
   return (
-    <div>
-    </div>
+    <>
+      <div className="bg-img" />
+      <div className="score">
+        <h3 className="score-high">High-score: {highScore}</h3>
+        <h3 className="score-current">Current score: {currentScore}</h3>
+      </div>
+      <div className="cards">
+        <Card imgSrc={cheddar} name="cheddar" />
+        <Card imgSrc={swiss} name="swiss" />
+        <Card imgSrc={american} name="american" />
+        <Card imgSrc={mozz} name="mozz" />
+        <Card imgSrc={colby} name="colby" />
+        <Card imgSrc={parm} name="parmesan" />
+      </div>
+    </>
   );
 }
 
