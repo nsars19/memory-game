3a474555d85a9624f5da8a6e98d129b0a6f3f337
Nick Sardonini <nsardonini@gmail.com>
2021-01-28 21:24:31 -0500

    extract loading element into its own component

---
+++ src/components/Loading.js
@@ -0,0 +1,20 @@
+const Loading = () => {
+  return (
+    <div className="load-wrap">
+      <h1 className="loading">
+        <span id="ld-0">L</span>
+        <span id="ld-1">o</span>
+        <span id="ld-2">a</span>
+        <span id="ld-3">d</span>
+        <span id="ld-4">i</span>
+        <span id="ld-5">n</span>
+        <span id="ld-6">g</span>
+        <span id="ld-7"> .</span>
+        <span id="ld-8"> .</span>
+        <span id="ld-9"> .</span>
+      </h1>
+    </div>
+  )
+}
+
+export default Loading;
