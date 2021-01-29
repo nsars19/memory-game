91cae6b8dff0efbd92f96526436947f34d75fbdf
Nick Sardonini <nsardonini@gmail.com>
2021-01-28 21:24:49 -0500

    add onClick prop to Card component
    when a card is clicked a new set of cards is selected for display, and then rendered

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 const Card = (props) => {
   return (
-    <div className="card">
+    <div className="card" onClick={props.onClick}>
       <div className="card-img">
         <img src={props.imgSrc} />
       </div>
