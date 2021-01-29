3fc106d3fb5afc5600e88223143b04ed879cd73c
Nick Sardonini <nsardonini@gmail.com>
2021-01-29 14:23:11 -0500

    add showHelp function to toggle game rules

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 
 const Header = (props) => {
   const [animationName, setAnimationName] = useState("changeColor");
+  const [iconStyle, setIconStyle] = useState("grid");
+  const [textStyle, setTextStyle] = useState("none");
 
   useEffect(() => {
     if (props.highScore === 0) return;
@@ -14,9 +16,25 @@ const Header = (props) => {
     setAnimationName(newName);
   }, [props.highScore]);
 
+  const showHelp = () => {
+    const help = document.getElementById("help");
+    const helpIcon = document.querySelector(".help-icon");
+    const helpText = document.querySelector(".help-text");
+
+    setIconStyle(iconStyle === "grid" ? "none" : "grid");
+    setTextStyle(textStyle === "none" ? "block" : "none");
+
+    helpIcon.style.display = iconStyle;
+    helpText.style.display = textStyle;
+  };
+
   return (
     <>
       <div className="bg-img" />
+      <div id="help" className="help">
+        <div className="help-icon" onClick={showHelp}><h2>?</h2></div>
+        <p className="help-text" onClick={showHelp}>Select cheeses you haven't selected yet! If you pick one that you've already selected the game restarts!</p>
+      </div>
       <div className="score">
         <h3 className="score-high">High-score: {props.highScore}</h3>
         <h3 className="score-current">Current score: {props.currentScore}</h3>
