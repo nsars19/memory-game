293113d719f150d956a09c4cc448750f41894aae
Nick Sardonini <nsardonini@gmail.com>
2021-01-29 12:06:21 -0500

    add hook to watch highScore prop for changes
    on change an animation name is added to the highScore
    element, which changes the text color from white -> red -> white, and wiggles
    the element.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,19 @@
+import { useState, useEffect } from "react";
+
 const Header = (props) => {
+  const [animationName, setAnimationName] = useState("changeColor");
+
+  useEffect(() => {
+    if (props.highScore === 0) return;
+
+    const highScore = document.querySelector(".score-high");
+    highScore.style.animationName = animationName;
+    
+    const newName = animationName === "changeColor" ? "changeColor2" : "changeColor";
+
+    setAnimationName(newName);
+  }, [props.highScore]);
+
   return (
     <>
       <div className="bg-img" />
